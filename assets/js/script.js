5dc4ade98559c8401f0c468cb41456a3c866bd82
FADLI-Said <saidfadli224@gmail.com>
2024-12-10 16:40:26 +0100

    Factoriser

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,23 +3,44 @@ document.getElementById("liveButton").addEventListener("click", function() {
     document.getElementById("liveText").innerText = texte
 })
 
+// document.getElementById("blueText").addEventListener("click", function () {
+//     let blue = document.getElementById("liveText")
+//     blue.removeAttribute("class")
+//     blue.classList.value("text-primary","text-success", "text-danger")
+//     blue.classList.value=""
+//     blue.classList.add("text-primary")
+// })
+
+// document.getElementById("greenText").addEventListener("click", function () {
+//     let blue = document.getElementById("liveText")
+//     blue.removeAttribute("class")
+//     blue.classList.remove("text-primary","text-success", "text-danger")
+//     blue.classList.value=""
+//     blue.classList.add("text-success")
+// })
+
+// document.getElementById("redText").addEventListener("click", function () {
+//     let blue = document.getElementById("liveText")
+//     blue.removeAttribute("class")
+//     blue.classList.remove("text-primary","text-success", "text-danger")
+//     blue.classList.value=""
+//     blue.classList.add("text-danger")
+// })
+
+function updateClass(className) {
+    let textcolor = document.getElementById("liveText")
+    textcolor.removeAttribute("class")
+    textcolor.classList.add(className)
+}
+
 document.getElementById("blueText").addEventListener("click", function () {
-    let blue = document.getElementById("liveText")
-    // blue.removeAttribute('class')
-    blue.classList.remove("text-primary","text-success", "text-danger")
-    blue.classList.add("text-primary")
+    updateClass("text-primary")
 })
 
 document.getElementById("greenText").addEventListener("click", function () {
-    let blue = document.getElementById("liveText")
-    // blue.removeAttribute('class')
-    blue.classList.remove("text-primary","text-success", "text-danger")
-    blue.classList.add("text-success")
+    updateClass("text-success")
 })
 
 document.getElementById("redText").addEventListener("click", function () {
-    let blue = document.getElementById("liveText")
-    // blue.removeAttribute('class')
-    blue.classList.remove("text-primary","text-success", "text-danger")
-    blue.classList.add("text-danger")
+    updateClass("text-danger")
 })
